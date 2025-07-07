ca4a44ee588ac994ae36814a10b9c8d5787f4537
Mohamed <fouademail@gmail.com>
2025-07-07 14:33:12 +0300

    setup project

---
+++ admindash-app/src/routes/ProtectedRoute.jsx
@@ -0,0 +1,5 @@
+
+// add role for only admin can access the dashboard
+// if (user?.role !== "admin") {
+//   return <Navigate to="/login" />
+// }
---
+++ admindash-app/src/services/firebase.js
@@ -0,0 +1,20 @@
+
+
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJ_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
